88102ead8e2b11aed65f354d8ebe3f95da8ec1d2
Alexey Alekhin <laughedelic@gmail.com>
2013-08-27 12:28:38 +0200

    Wrote install method; tested

--- src/main/scala/ZlibDevel.scala
+++ src/main/scala/ZlibDevel.scala
@@ -1,14 +1,16 @@
 package ohnosequences.statika
 
 import ohnosequences.statika._
+import sys.process._
 
 case object ZlibDevel extends Bundle() {
 
   val metadata = generated.metadata.ZlibDevel
 
   def install[D <: DistributionAux](distribution: D): InstallResults = {
-    // do someting here
-    success("%s is installed" format metadata)
+    val result = Seq("yum", "install", "zlib-devel", "-y").!
+    if (result == 0) success(metadata+" is installed")
+    else failure("Couldn't install "+metadata)
   }
 
 }
