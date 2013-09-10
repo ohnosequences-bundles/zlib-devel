b25fd529fad25c24588b3dd8c351000b9a5a5434
Alexey Alekhin <laughedelic@gmail.com>
2013-09-10 19:47:59 +0200

    Updated to statika-0.15-snapshot with new install methods

--- build.sbt
+++ build.sbt
@@ -4,9 +4,15 @@ description := "A bundle for zlib-devel tool"
 
 homepage := Some(url("https://github.com/ohnosequences/zlib-devel"))
 
+organization := "ohnosequences"
+
+organizationHomepage := Some(url("http://ohnosequences.com"))
+
 licenses := Seq("AGPLv3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
 
-// fully qualified name(s) of your bundle object(s)
 bundleObjects := Seq("ohnosequences.statika.ZlibDevel")
 
- 
+
+statikaVersion := "0.15.0-SNAPSHOT"
+
+awsStatikaVersion := "0.2.0-SNAPSHOT"
--- src/main/scala/ZlibDevel.scala
+++ src/main/scala/ZlibDevel.scala
@@ -7,10 +7,7 @@ case object ZlibDevel extends Bundle() {
 
   val metadata = generated.metadata.ZlibDevel
 
-  def install[D <: DistributionAux](distribution: D): InstallResults = {
-    val result = Seq("yum", "install", "zlib-devel", "-y").!
-    if (result == 0) success(metadata+" is installed")
-    else failure("Couldn't install "+metadata)
-  }
+  def install[D <: AnyDistribution](distribution: D): InstallResults =
+    Seq("yum", "install", "zlib-devel", "-y") ->- success(metadata+" is installed")
 
 }
